f5e26332d35f8ad30e66d81942e4bb31cccde8d5
lj <liujing@anchol.com>
2017-08-11 11:31:36 +0800

    merge capital thrift module

---
+++ durian-user-client/src/main/java/com/durian/user/capital/client/UserCapitalClientConfig.java
@@ -0,0 +1,25 @@
+package com.durian.user.capital.client;
+
+import com.durian.user.capital.thrift.api.service.UserCapitalServiceApi;
+import com.platform.common.thrift.client.annotation.ThriftLoadBalancerClient;
+import com.platform.common.thrift.client.config.HttpThriftClientAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Scope;
+
+/**
+ * Created by lj on 2017/8/11.
+ */
+@Configuration
+@Import(HttpThriftClientAutoConfiguration.class)
+public class UserCapitalClientConfig {
+		@ThriftLoadBalancerClient(serviceId = "capital", url = "/api/capital")
+		private UserCapitalServiceApi.Iface userCapitalServiceApi;
+
+		@Bean
+		@Scope("prototype")
+		public UserCapitalServiceApi.Iface userCapitalServiceApi() {
+				return this.userCapitalServiceApi;
+		}
+}
