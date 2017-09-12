0a4a9db2d36edf67d47508ac092a3e5caae4ecce
daixibiao <daixibiao@anchol.com>
2017-09-12 20:40:14 +0800

    删除多余工具类

--- durian-user-dispatcher/src/main/java/com/durian/user/dispatcher/api/BackendUserServiceApiImpl.java
+++ durian-user-dispatcher/src/main/java/com/durian/user/dispatcher/api/BackendUserServiceApiImpl.java
@@ -1,7 +1,5 @@
 package com.durian.user.dispatcher.api;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.durian.user.domain.po.BackendUser;
 import com.durian.user.domain.to.LoginUser;
 import com.durian.user.domain.to.UserAllInfo;
@@ -17,14 +15,10 @@ import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -35,19 +29,6 @@ import java.util.stream.Collectors;
 public class BackendUserServiceApiImpl implements BackendUserServiceApi.Iface{
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BackendUserServiceApiImpl.class);
-
-    public static String[] getNullPropertyNames (Object source) {
-        final BeanWrapper src = new BeanWrapperImpl(source);
-        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
-
-        Set<String> emptyNames = new HashSet<String>();
-        for(java.beans.PropertyDescriptor pd : pds) {
-            Object srcValue = src.getPropertyValue(pd.getName());
-            if (srcValue == null) emptyNames.add(pd.getName());
-        }
-        String[] result = new String[emptyNames.size()];
-        return emptyNames.toArray(result);
-    }
 
     @Autowired
     private BackendUserService backendUserService;
