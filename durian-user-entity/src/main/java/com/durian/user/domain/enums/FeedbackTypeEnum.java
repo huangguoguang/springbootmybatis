e736b36c1207d59929e207c5d1c72afab0af13b1
wangli <wangli@anchol.com>
2017-10-20 11:10:03 +0800

    意见反馈、微信二维码枚举

---
+++ durian-user-entity/src/main/java/com/durian/user/domain/enums/FeedbackTypeEnum.java
@@ -0,0 +1,34 @@
+package com.durian.user.domain.enums;
+
+/**
+ * Created by wangli on 2017/10/20.
+ */
+public enum FeedbackTypeEnum {
+    CODE("1","二维码"),
+    FEEDBACK("2","意见反馈");
+
+    private String code;
+
+    private String type;
+
+    FeedbackTypeEnum(String code, String type) {
+        this.code = code;
+        this.type = type;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
